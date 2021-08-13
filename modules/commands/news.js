a4c31e8aaddd9cdc5fea76b91f38bdb50cf00674
tuilatoanne1 <85426939+tuilatoanne1@users.noreply.github.com>
2021-08-13 21:00:36 +0700

    Add files via upload

--- modules/commands/news.js
+++ modules/commands/news.js
@@ -1,6 +1,6 @@
 module.exports.config = {
     name: "news",
-    version: "1.0.1",
+    version: "1.0.0",
     hasPermssion: 0,
     credits: "Mirai Team",
     description: "Tin tức trên vnexpress.net",
@@ -14,25 +14,14 @@ module.exports.config = {
     }
 };
 
-module.exports.languages = {
-    "vi": {
-        "MissingInput": "Hãy nhập từ khóa bạn muốn tìm kiếm",
-        "notFoundResult": "Không có kết quả nào với từ khóa của bạn"
-    },
-    "en": {
-        "MissingInput": "Enter what you want to search ",
-        "notFoundResult": "There is no result match your input"
-    }
-}
-
-module.exports.run = async function({ api, event, args, getText }) {
+module.exports.run = async function({ api, event, args }) {
     const axios = global.nodemodule["axios"];
     const https = global.nodemodule["https"];
     const cheerio = global.nodemodule["cheerio"];
     var out = (msg) => api.sendMessage(msg, event.threadID, event.messageID);
     var url = "https://timkiem.vnexpress.net/?q=";
     var q = args.join(" ");
-    if (!q) return out(getText("missingInput"));
+    if (!q) return out("Hãy nhập từ khóa bạn muốn tìm kiếm");
 
     function certificate({ url }) {
         return new Promise(async function(resolve, reject) {
@@ -57,7 +46,7 @@ module.exports.run = async function({ api, event, args, getText }) {
     var data = await certificate({ url });
     var $ = cheerio.load(data);
 
-    if (!$('h3.title-news').eq(0).text()) return out(getText("notFoundResult"));
+    if (!$('h3.title-news').eq(0).text()) return out("Không có kết quả nào với từ khóa của bạn");
     for (let e = 0; e < 3; e++) {
         var title = JSON.stringify($('h3.title-news').eq(e).text()).replace(/\\n|\\t|\"/g, "");
         var desc = $('p.description').eq(e).text();
--- modules/commands/sendnoti.js
+++ modules/commands/sendnoti.js
@@ -1,6 +1,6 @@
 module.exports.config = {
 	name: "sendnoti",
-	version: "1.0.1",
+	version: "1.0.0",
 	hasPermssion: 2,
 	credits: "Mirai Team",
 	description: "Gửi tin nhắn tới các nhóm!",
@@ -9,18 +9,7 @@ module.exports.config = {
 	cooldowns: 5
 };
 
-module.exports.languages = {
-	"vi": {
-		"sendSuccess": "Đã gửi tin nhắn đến %1 nhóm!",
-		"sendFail": "[!] Không thể gửi thông báo tới %1 nhóm"
-	},
-	"en": {
-		"sendSuccess": "Sent message to %1 thread!",
-		"sendFail": "[!] Can't send message to %1 thread"
-	}
-}
-
-module.exports.run = async ({ api, event, args, getText }) => {
+module.exports.run = async ({ api, event, args }) => {
 	var allThread = global.data.allThreadID || [];
 	var count = 1,
 		cantSend = [];
@@ -34,5 +23,5 @@ module.exports.run = async ({ api, event, args, getText }) => {
 			await new Promise(resolve => setTimeout(resolve, 500));
 		}
 	}
-	return api.sendMessage(getText("sendSuccess", count), event.threadID, () => (cantSend.length > 0 ) ? api.sendMessage(getText("sendFail", cantSend.length), event.threadID, event.messageID) : "", event.messageID);
+	return api.sendMessage(`Đã gửi tin nhắn đến ${count} nhóm!`, event.threadID, () => (cantSend.length > 0 ) ? api.sendMessage(`[!] Không thể gửi thông báo tới ${cantSend.length} nhóm`, event.threadID, event.messageID) : "", event.messageID);
 }
